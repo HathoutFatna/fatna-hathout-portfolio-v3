aba0f55e718f8f9eed4d7c18845d354509391248
Fatna Hathout <f.hathout@outlook.fr>
2025-08-23 10:41:48 +0100

    feat: Add Contact section

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -2,9 +2,19 @@ import React from "react";
 
 function Contact() {
   return (
-    <div id="contact" className="h-screen">
-      Contact
-    </div>
+    <section id="contact" className="contact">
+      <p className="contact-subtitle">Get In Touch</p>
+      <h2 className="contact-title">Always Open to New Challenges</h2>
+      <p>
+        I’m always open to new ideas, collaborations, or challenges. If you
+        think we could create something great together, feel free to reach out!
+      </p>
+      <a href="mailto:fatnahathout@gmail.com" className="mb-20">
+        <div className="contact-cta">
+          <span>Say Hello</span>
+        </div>
+      </a>
+    </section>
   );
 }
 
